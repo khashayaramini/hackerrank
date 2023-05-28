fe2871c407514dccfb4f37afbd8ebfa416120f41
khashayaramini <khashayaramini80@gmail.com>
2023-05-28 17:28:38 +0300

    c intro solved

---
+++ c/pointers_in_c.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+    int old_a = *a;
+    int old_b = *b;
+    *a = old_a + old_b;
+    int diff = old_a - old_b;
+    *b = diff > 0 ? diff : diff * -1;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
