34f473076d9065962004e266a6741034dde8c8f9
khashayaramini <khashayaramini80@gmail.com>
2023-05-29 23:45:16 +0300

    indent

--- c/functions_in_c.c
+++ c/functions_in_c.c
@@ -3,21 +3,21 @@
 Add `int max_of_four(int a, int b, int c, int d)` here.
 */
 int max_of_four(int a, int b, int c, int d){
-    int max = a;
-    if(b > max)
-        max = b;
-    if(c > max)
-        max = c;
-    if(d > max)
-        max = d;
-    return max;
+        int max = a;
+        if(b > max)
+                max = b;
+        if(c > max)
+                max = c;
+        if(d > max)
+                max = d;
+        return max;
 }
 
 int main() {
-    int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
-    int ans = max_of_four(a, b, c, d);
-    printf("%d", ans);
-    
-    return 0;
+        int a, b, c, d;
+        scanf("%d %d %d %d", &a, &b, &c, &d);
+        int ans = max_of_four(a, b, c, d);
+        printf("%d", ans);
+
+        return 0;
 }
--- c/playing_with_characters.c
+++ c/playing_with_characters.c
@@ -6,15 +6,15 @@
 int main() 
 {
 
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    char ch;
-    char s[100];
-    char sen[100];
-    scanf("%c", &ch);
-    scanf("\n");
-    scanf("%[^\n]%*c", s);
-    scanf("\n");
-    scanf("%[^\n]%*c", sen);
-    printf("%c\n%s\n%s\n", ch, s, sen);
-    return 0;
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+        char ch;
+        char s[100];
+        char sen[100];
+        scanf("%c", &ch);
+        scanf("\n");
+        scanf("%[^\n]%*c", s);
+        scanf("\n");
+        scanf("%[^\n]%*c", sen);
+        printf("%c\n%s\n%s\n", ch, s, sen);
+        return 0;
 }
